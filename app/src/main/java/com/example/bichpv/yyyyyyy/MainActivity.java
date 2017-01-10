cf98cd0ee072b2bb7a87335d76bbd72445b70575
BichPV <vanbich148@gmail.com>
2017-01-10 22:05:29 +0700

    uuuu

--- app/src/main/java/com/example/bichpv/yyyyyyy/MainActivity.java
+++ app/src/main/java/com/example/bichpv/yyyyyyy/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        ///kasjdkoahsdioahsdioahsd
+        //// STOPSHIP: 1/10/2017  
     }
 }
